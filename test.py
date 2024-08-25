8bf245032ed528003f6cbfdfa5e70b0356983939
Alexander Stathopoulos <alexstath011@gmail.com>
2024-08-25 21:38:12 +0930

    added json schema

--- test.py
+++ test.py
@@ -9,37 +9,91 @@ client = OpenAI(
 
 )
 
+pdf = """
+Product data sheet
+Specifications
+Wear part, fan for soft starter,
+subassembly, Altistar 22, 110V, size
+E
+VZ3V22E1110V
+Main
+Range compatibility Altistar 22
+Accessory / separate part type Fan kit
+Accessory / separate part
+category
+Cooling accessory
+Product or component type Fan
+Complementary
+Kit composition Fan
+Mounting bracket
+Instruction sheet
+Compatibility with soft starter Soft starter ATS22 110...115 V (size: 304 x 340 x 455 mm)
+Accessory / separate part
+destination
+Soft starter
+Packing Units
+Unit Type of Package 1 PCE
+Number of Units in Package 1 1
+Package 1 Height 11.500 cm
+Package 1 Width 30.000 cm
+Package 1 Length 40.000 cm
+Package 1 Weight 2.192 kg
+Unit Type of Package 2 P06
+Number of Units in Package 2 10
+Package 2 Height 75.000 cm
+Package 2 Width 60.000 cm
+Package 2 Length 80.000 cm
+Package 2 Weight 34.920 kg
+Recommended replacement(s)
+Disclaimer: This documentation is not intended as a substitute for and is not to be used for determining suitability or reliability of these products for specific user applications
+Jan 18, 2023 1
+"""
+
 response = client.chat.completions.create(
     model="gpt-4o-2024-08-06",
-    messages=[{"role": "system", "content": "You are a helpful math tutor. Guide the user through the solution step by step."},
-              {"role": "user", "content": "how can I solve 8x + 7 = -23"}],
+    messages=[{"role": "system", "content": "You are a helpful data entry assistant. Extract the relevant values out of provided pdf documents and output in json."},
+              {"role": "user", "content": pdf}],
     response_format={
         "type": "json_schema",
         "json_schema": {
-            "name": "steps",
+            "name": "document_response",
             "schema": {
                 "type": "object",
                 "properties": {
-                    "steps": {
+                    "documents": {
                         "type": "array",
                         "items": {
                             "type": "object",
                             "properties": {
-                                "explanation": {"type": "string"},
-                                "output": {"type": "string"}
+                                "documentTitle": {
+                                    "type": "string"
+                                },
+                                "content": {
+                                    "type": "object",
+                                    "properties": {
+                                        "name": {
+                                            "type": "string"
+                                        },
+                                        "value": {
+                                            "type": "string"
+                                        }
+                                    },
+                                    "required": ["name", "value"],
+                                    "additionalProperties": False
+                                }
                             },
-                            "required": ["explanation", "output"],
+                            "required": ["documentTitle", "content"],
                             "additionalProperties": False
                         }
-                    },
-                    "final_answer": {"type": "string"}
+                    }
                 },
-                "required": ["steps", "final_answer"],
+                "required": ["documents"],
                 "additionalProperties": False
             },
             "strict": True
         }
     }
+
 )
 
 print(response.choices[0].message.content)
